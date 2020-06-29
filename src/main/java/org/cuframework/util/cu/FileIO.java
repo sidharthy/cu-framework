601ee86e7ee52dab00b82106449cd403ace7fa4f
Sidharth Yadav <sidharth_08@yahoo.com>
2020-06-29 19:31:52 +0530

    Added support for fs operations ls, cp, mv, rename, rm

--- src/main/java/org/cuframework/util/cu/FileIO.java
+++ src/main/java/org/cuframework/util/cu/FileIO.java
@@ -29,6 +29,8 @@
 package org.cuframework.util.cu;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileFilter;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.InputStream;
@@ -36,6 +38,10 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.Reader;
+
+import java.nio.file.Files;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -51,8 +57,9 @@ public class FileIO extends HeadlessExecutableGroup implements IExecutable {
 
     //cu input parameters
     private static final String PARAM_OPERATION = "operation";
-    private static final String PARAM_PATH = "path";  //path of file to read/write
-    private static final String PARAM_TYPE = "type";  //file type e.g. text
+    private static final String PARAM_PATH = "path";  //path of file/directory to read/write. It must either be of type File or String.
+    private static final String PARAM_TARGET_PATH = "target";  //target of copy and move operations.
+    private static final String PARAM_TYPE = "type";  //file type e.g. text. If the operation specified is 'ls' then this will represent the file filter.
     private static final String PARAM_IO_MODE = "io-mode";  //read, write
     private static final String PARAM_WRITE_MODE = "write-mode";  //e.g. append
     private static final String PARAM_STREAM = "stream";  //holds an object of io stream
@@ -72,6 +79,16 @@ public class FileIO extends HeadlessExecutableGroup implements IExecutable {
         Map<String, Object> resultMap = new HashMap<>();
         try {
             switch(operation.toUpperCase().trim()) {
+                case "LS": resultMap.put(resultKeyName, listItems(requestContext)); break;
+                case "CP":
+                case "COPY": resultMap.put(resultKeyName, copyItem(requestContext)); break;
+                case "MV":
+                case "MOVE": resultMap.put(resultKeyName, moveItem(requestContext)); break;
+                case "REN":
+                case "RENAME": resultMap.put(resultKeyName, renameItem(requestContext)); break;
+                case "RM":
+                case "DEL":
+                case "DELETE": resultMap.put(resultKeyName, deleteItem(requestContext)); break;
                 case "OPEN": resultMap.put(resultKeyName, openFile(requestContext)); break;
                 case "READ": resultMap.put(resultKeyName, readFile(requestContext)); break;
                 case "WRITE": writeFile(requestContext); break;
@@ -87,20 +104,126 @@ public class FileIO extends HeadlessExecutableGroup implements IExecutable {
         return resultMapKeyName;
     }
 
+    //Returns null if the path to be inspected is null or represents a non existent location.
+    //Returns an array of File objects, matching the type filter (if one provided), if the path represented a directory.
+    //Returns the File object, matching the type filter (if one provided), if the path represented a file.
+    //Else returns null.
+    private Object listItems(Map<String, Object> requestContext) {
+        Object path = requestContext.get(PARAM_PATH);
+        if (path == null) {
+            return null;
+        }
+
+        //path must represent a Path, File or a String object
+        File f = path instanceof File? (File) path:
+                 path instanceof Path? ((Path) path).toFile(): new File((String) path);
+
+        if (!f.exists()) {
+            return null;
+        }
+        String fileFilter = (String) requestContext.get(PARAM_TYPE);
+        if (f.isDirectory()) {
+            return f.listFiles(new FileFilter() {
+                public boolean accept(File f) {
+                    return fileFilter == null? true: f.getName().matches(fileFilter);
+                }
+            });
+        } else {
+            return fileFilter == null || f.getName().matches(fileFilter)? f: null;
+        }
+    }
+
+    //returns null if the source or target was null. Else it returns the Path to the copied file.
+    private Object copyItem(Map<String, Object> requestContext) throws IOException {
+        Path[] paths = getSourceAndTargetAsPaths(requestContext);
+        Path source = paths[0];
+        Path target = paths[1];
+
+        if (source == null || target == null) {
+            return null;
+        }
+
+        boolean isTargetDirectory = target.toFile().isDirectory();
+        return Files.copy(source, isTargetDirectory? target.resolve(source.getFileName()): target);  //TODO add recursion support
+    }
+
+    //returns null if the source or target was null. Else it returns the Path to the moved file.
+    private Object moveItem(Map<String, Object> requestContext) throws IOException {
+        Path[] paths = getSourceAndTargetAsPaths(requestContext);
+        Path source = paths[0];
+        Path target = paths[1];
+
+        if (source == null || target == null) {
+            return null;
+        }
+
+        boolean isTargetDirectory = target.toFile().isDirectory();
+        return Files.move(source, isTargetDirectory? target.resolve(source.getFileName()): target);  //TODO add recursion support
+    }
+
+    //returns null if rename was not attempted else returns the Path to the renamed file.
+    private Object renameItem(Map<String, Object> requestContext) throws IOException {
+        Path source = getAsPath(PARAM_PATH, requestContext);
+        String newName = (String) requestContext.get(PARAM_TARGET_PATH);  //object must be of type String
+
+        if (source == null || newName == null) {
+            return null;
+        }
+
+        return Files.move(source, source.resolveSibling(newName));
+    }
+
+    //returns null if source was null. Else it returns true/false as per the outcome of the delete operation.
+    private Object deleteItem(Map<String, Object> requestContext) throws IOException {
+        Path source = getAsPath(PARAM_PATH, requestContext);
+
+        if (source == null) {
+            return null;
+        }
+
+        return Files.deleteIfExists(source);  //TODO add recursion support
+    }
+
+    //0th index contains the path to source and 1st index contains path to target
+    private Path[] getSourceAndTargetAsPaths(Map<String, Object> requestContext) {
+        Path[] paths = new Path[2];
+        paths[0] = getAsPath(PARAM_PATH, requestContext);
+        paths[1] = getAsPath(PARAM_TARGET_PATH, requestContext);
+        return paths;
+    }
+
+    private Path getAsPath(String attributeName, Map<String, Object> requestContext) {
+        Object path = requestContext.get(attributeName);
+        Path asPath = null;
+
+        if (path != null) {
+            //path must represent a Path, File or a String object
+            asPath = path instanceof Path? (Path) path:
+                     path instanceof File? ((File) path).toPath(): FileSystems.getDefault().getPath((String) path);
+        }
+
+        return asPath;
+    }
+
     private Object openFile(Map<String, Object> requestContext) throws IOException {
-        String path = (String) requestContext.get(PARAM_PATH);
+        Object path = requestContext.get(PARAM_PATH);
         String type = (String) requestContext.get(PARAM_TYPE);
         boolean textFileType = "text".equalsIgnoreCase(type);
         String ioMode = (String) requestContext.get(PARAM_IO_MODE);
         if (ioMode == null) {
             return null;
         }
+
+        //path must represent a Path, File or a String object
+        File f = path instanceof File? (File) path:
+                 path instanceof Path? ((Path) path).toFile(): new File((String) path);
+
         switch(ioMode.toUpperCase().trim()) {
-            case "READ": return textFileType? new BufferedReader(new InputStreamReader(new FileInputStream(path))): new FileInputStream(path);
+            case "READ": return textFileType? new BufferedReader(new InputStreamReader(new FileInputStream(f))): new FileInputStream(f);
             case "WRITE":
                          {
                              String writeMode = (String) requestContext.get(PARAM_WRITE_MODE);
-                             return new FileOutputStream(path, writeMode == null? true: "append".equalsIgnoreCase(writeMode));
+                             return new FileOutputStream(f, writeMode == null? true: "append".equalsIgnoreCase(writeMode));
                          }
             default: throw new UnsupportedOperationException("Unsupported io mode: " + ioMode);
         }
